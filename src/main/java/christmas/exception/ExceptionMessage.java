f16ba3e719683a3558cfe347fd7bce69114a01c3
ahma0 <dsd932@naver.com>
2023-11-11 01:11:42 +0900

    feat: ExceptionMessage.java 수정

--- src/main/java/christmas/exception/ExceptionMessage.java
+++ src/main/java/christmas/exception/ExceptionMessage.java
@@ -2,9 +2,9 @@ package christmas.exception;
 
 public class ExceptionMessage {
 
-    public static final String NOT_DIGIT = "[ERROR] 숫자를 입력해주세요.";
-    public static final String NOT_EMPTY_OR_BLANK = "[ERROR] 공백이나 빈 칸이 포함되어 있습니다.";
-    public static final String NOT_EXCEEDED = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해주세요.";
+    public static final String NOT_DIGIT = "[ERROR] 숫자를 입력해주세요. 다시 입력해 주세요.";
+    public static final String NOT_EMPTY_OR_BLANK = "[ERROR] 공백이나 빈 칸이 포함되어 있습니다. 다시 입력해 주세요.";
+    public static final String NOT_EXCEEDED = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
     public static final String NOI_IN_MENU = "[ERROR] ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
 
 }
