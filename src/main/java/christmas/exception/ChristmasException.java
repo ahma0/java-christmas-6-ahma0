fc7c807032796f6468ca67fba28ae0fbacda35ab
ahma0 <dsd932@naver.com>
2023-11-11 20:42:36 +0900

    refactor: exception 로직 수정

--- src/main/java/christmas/exception/ChristmasException.java
+++ src/main/java/christmas/exception/ChristmasException.java
@@ -8,4 +8,22 @@ public class ChristmasException {
         }
     }
 
+    public static void causeIllegalArgumentExceptionForDate(boolean condition) {
+        if (condition) {
+            throw new IllegalArgumentException(ExceptionMessage.NOT_VALID_DATE);
+        }
+    }
+
+    public static void causeIllegalArgumentExceptionForOrderDetails(boolean condition) {
+        if (condition) {
+            throw new IllegalArgumentException(ExceptionMessage.NOT_VALID_ORDER);
+        }
+    }
+
+    public static void causeIllegalStateExceptionForOrderDetails(boolean condition) {
+        if (condition) {
+            throw new IllegalArgumentException(ExceptionMessage.NOT_VALID_ORDER);
+        }
+    }
+
 }
