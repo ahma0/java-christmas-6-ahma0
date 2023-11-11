7b9efeac55ddb4ef5465d6132c56dd35876449d6
ahma0 <dsd932@naver.com>
2023-11-11 20:44:37 +0900

    feat: 메뉴와 개수를 저장, 검증하는 로직 구현

--- src/main/java/christmas/model/Reservation.java
+++ src/main/java/christmas/model/Reservation.java
@@ -1,16 +1,20 @@
 package christmas.model;
 
-import christmas.exception.ExceptionMessage;
+import christmas.model.value.CourseMeal;
+import christmas.model.value.Menu;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
-import static christmas.exception.ChristmasException.causeIllegalArgumentException;
+import static christmas.exception.ChristmasException.*;
 
 public class Reservation {
 
     private final LocalDate reservationDate;
-    private Map<Enum<?>, Integer> orderDetails;
+    private Map<Menu, Integer> orderDetails;
 
     public Reservation(String reservationDate) {
         validateReservationDate(reservationDate);
@@ -26,26 +30,65 @@ public class Reservation {
         return reservationDate;
     }
 
-    public Map<Enum<?>, Integer> getOrderDetails() {
+    public Map<Menu, Integer> getOrderDetails() {
         return orderDetails;
     }
 
-    public void setOrderDetails(Map<Enum<?>, Integer> orderDetails) {
-        this.orderDetails = orderDetails;
+    public void setOrderDetails(String orders) {
+        validateReservationOrderDetails(orders);
+        this.orderDetails = parseOrderDetails(orders);
+    }
+
+    private static Map<Menu, Integer> parseOrderDetails(String orders) {
+        return Arrays.stream(orders.split(","))
+                .map(pair -> pair.split("-"))
+                .collect(Collectors.toMap(
+                        keyValue -> CourseMeal.findMenuItemByName(keyValue[0]),
+                        keyValue -> Integer.parseInt(keyValue[1])
+                ));
     }
 
     private void validateReservationDate(String day) {
-        causeIllegalArgumentException(isBlankOrEmpty(day), ExceptionMessage.NOT_EMPTY_OR_BLANK);
-        causeIllegalArgumentException(!isDigit(day), ExceptionMessage.NOT_DIGIT);
-        causeIllegalArgumentException(isDateExceeded(day), ExceptionMessage.NOT_EXCEEDED);
+        causeIllegalArgumentExceptionForDate(
+                isBlankOrEmpty(day) || ! isDigit(day) || isDateExceeded(day)
+        );
+    }
+
+    private void validateReservationOrderDetails(String orders) {
+        causeIllegalStateExceptionForOrderDetails(isBlankOrEmptyByOrderDetails(orders));
+        causeIllegalArgumentExceptionForOrderDetails(isDuplicated(orders));
+        validateOrderDetails(orders);
+    }
+
+    private void validateOrderDetails(String orders) {
+        Arrays.stream(orders.replaceAll(" ", "").split(","))
+                .map(pair -> pair.split("-"))
+                .forEach(pair -> causeIllegalArgumentExceptionForOrderDetails(isDigit(pair[1]) || isZero(pair[1])));
+    }
+
+    private boolean isBlankOrEmptyByOrderDetails(String orders) {
+        return Arrays.stream(orders.split(",|-"))
+                .anyMatch(order -> order.isBlank() || order.isEmpty());
     }
 
     private boolean isBlankOrEmpty(String day) {
         return day.isEmpty() || day.isBlank();
     }
 
-    private boolean isDigit(String day) {
-        return day.matches("-?\\d+");
+    private boolean isDuplicated(String orders) {
+        List<String> collect = Arrays.stream(orders.replaceAll(" ", "").split(","))
+                .map(pair -> pair.split("-")[0])
+                .toList();
+
+        return collect.stream().distinct().count() != collect.size();
+    }
+
+    private boolean isZero(String str) {
+        return Integer.parseInt(str) == 0;
+    }
+
+    private boolean isDigit(String str) {
+        return str.matches("-?\\d+");
     }
 
     private boolean isDateExceeded(String day) {
