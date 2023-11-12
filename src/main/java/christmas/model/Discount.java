ef3c2c6474a1a541f500925d4261e384669d9580
ahma0 <dsd932@naver.com>
2023-11-12 23:41:34 +0900

    feat: 할인 전 총 주문 금액에 따라 증정 메뉴를 반환하는 기능 구현

--- src/main/java/christmas/model/Discount.java
+++ src/main/java/christmas/model/Discount.java
@@ -6,6 +6,8 @@ import christmas.model.value.Menu;
 public class Discount {
 
     private static final Menu GIVEAWAY = Beverage.CHAMPAGNE;
+    private static final int GIVEAWAY_CRITERIA = 120_000;
+    private static final String NOT_EXIST = "없음";
 
     private final ChristmasEvent christmasEvent;
     private final DecemberEvent decemberEvent;
@@ -21,5 +23,16 @@ public class Discount {
 
     public int getTotalPrice() {
         return totalPrice;
+    }
+
+    public String getGiveaway() {
+        if (didTotalPriceMeetCriteria()) {
+            return String.format("%s %d개", GIVEAWAY.getMenuName(), 1);
+        }
+        return NOT_EXIST;
+    }
+
+    private boolean didTotalPriceMeetCriteria() {
+        return totalPrice >= GIVEAWAY_CRITERIA;
     }
 }
