3a5ae277b4322f1cdd0a852d6f1fdc773f7f3932
ahma0 <dsd932@naver.com>
2023-11-12 22:45:21 +0900

    refactor: OrderDetails 일급 컬렉션으로 분리

--- src/main/java/christmas/model/Reservation.java
+++ src/main/java/christmas/model/Reservation.java
@@ -1,23 +1,15 @@
 package christmas.model;
 
-import christmas.model.value.CourseMeal;
-import christmas.model.value.Menu;
-
 import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.HashSet;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 import static christmas.exception.ChristmasException.*;
 
 public class Reservation {
 
-    private static final int MAX_ORDER = 20;
-
     private final LocalDate reservationDate;
-    private Map<Menu, Integer> orderDetails;
+    private OrderDetails orderDetails;
 
     public Reservation(String reservationDate) {
         validateReservationDate(reservationDate);
@@ -34,23 +26,15 @@ public class Reservation {
     }
 
     public List<String> getOrderDetailsWithFormat() {
-        return orderDetails.entrySet().stream()
-                .map(entry -> String.format("%s %d개", entry.getKey().getMenuName(), entry.getValue()))
-                .collect(Collectors.toList());
+        return orderDetails.getOrderDetailsWithFormat();
+    }
+
+    public String getReservationDateWithFormat() {
+        return reservationDate.format(DateTimeFormatter.ofPattern("MM월 dd일"));
     }
 
     public void setOrderDetails(String orders) {
-        validateReservationOrderDetails(orders);
-        this.orderDetails = parseOrderDetails(orders);
-    }
-
-    private static Map<Menu, Integer> parseOrderDetails(String orders) {
-        return Arrays.stream(orders.split(","))
-                .map(pair -> pair.split("-"))
-                .collect(Collectors.toMap(
-                        keyValue -> CourseMeal.findMenuItemByName(keyValue[0]),
-                        keyValue -> Integer.parseInt(keyValue[1])
-                ));
+        orderDetails = new OrderDetails(orders);
     }
 
     private void validateReservationDate(String day) {
@@ -59,57 +43,8 @@ public class Reservation {
         );
     }
 
-    private void validateReservationOrderDetails(String orders) {
-        causeIllegalStateExceptionForOrderDetails(isBlankOrEmptyByOrderDetails(orders));
-        causeIllegalArgumentExceptionForOrderDetails(
-                isDuplicated(orders)
-                        || isAllBeverage(orders)
-                        || hasOrderExceededMaximumQuantity(orders)
-        );
-        validateOrderDetails(orders);
-    }
-
-    private void validateOrderDetails(String orders) {
-        Arrays.stream(orders.replaceAll(" ", "").split(","))
-                .map(pair -> pair.split("-"))
-                .forEach(pair -> causeIllegalArgumentExceptionForOrderDetails(!isDigit(pair[1]) || isZero(pair[1])));
-    }
-
-    private boolean isBlankOrEmptyByOrderDetails(String orders) {
-        return Arrays.stream(orders.split(",|-"))
-                .anyMatch(this::isBlankOrEmpty);
-    }
-
     private boolean isBlankOrEmpty(String str) {
         return str.isEmpty() || str.isBlank();
-    }
-
-    private boolean isDuplicated(String orders) {
-        List<String> collect = Arrays.stream(orders.replaceAll(" ", "").split(","))
-                .map(pair -> pair.split("-")[0])
-                .toList();
-
-        return collect.size() != new HashSet<>(collect).size();
-    }
-
-    private boolean hasOrderExceededMaximumQuantity(String orders) {
-        return Arrays.stream(orders.replaceAll(" ", "").split(","))
-                .map(pair -> pair.split("-")[1])
-                .mapToInt(Integer::parseInt)
-                .sum() > MAX_ORDER;
-    }
-
-    private boolean isAllBeverage(String orders) {
-        List<Menu> menuNames = Arrays.stream(orders.replaceAll(" ", "").split(","))
-                .map(pair -> pair.split("-")[0])
-                .map(CourseMeal::findMenuItemByName)
-                .toList();
-
-        return new HashSet<>(CourseMeal.BEVERAGE.getMenus()).containsAll(menuNames);
-    }
-
-    private boolean isZero(String str) {
-        return Integer.parseInt(str) == 0;
     }
 
     private boolean isDigit(String str) {
